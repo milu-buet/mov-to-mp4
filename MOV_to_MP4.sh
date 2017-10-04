990f34da866fb67d11bdee5ed08c1d2d7526d8c8
Lutfar Rahman Milu <milu.buet@gmail.com>
2017-10-04 18:04:37 -0500

    Update MOV_to_MP4.sh

--- MOV_to_MP4.sh
+++ MOV_to_MP4.sh
@@ -1,3 +1,10 @@
+#Author- Milu
+
+#Keep all the MOV file in a folder as "./ins"
+#Run  ./MOV_to_MP4.sh 
+#It will convert all the files in "./ins" and save to "./outs"
+# sudo apt-get install ffmpeg
+
 mkdir "outs"
 
 for entry in "./ins"/*
@@ -5,4 +12,5 @@ do
   echo "$entry"
   name=$(basename "$entry" ".MOV")
   ffmpeg -i $entry -strict -2 -vcodec copy ./outs/$name.mp4
+  # sudo apt-get install ffmpeg
 done
